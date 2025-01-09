caa4df1a365ed4aa41d573b21ebdade144471e97
abh1nav9 <ABHINAVGAUTAM.210111063@gehu.ac.in>
2025-01-09 17:30:24 +0530

    Added : DB Connection and ENV Proxy

--- backend/src/connection.ts
+++ backend/src/connection.ts
@@ -1,1 +1,14 @@
-// DB Connection
+import mongoose from 'mongoose';
+import {MONGO_URI} from './proxy';
+
+const MONGODB_URI = MONGO_URI || "String Not Available";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
