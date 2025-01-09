51595ef1d61cf6fd8d9c2af898ce82727d7d8504
abh1nav9 <ABHINAVGAUTAM.210111063@gehu.ac.in>
2025-01-09 17:35:18 +0530

    Added : Search Log Schema

--- backend/src/models/search.model.ts
+++ backend/src/models/search.model.ts
@@ -1,1 +1,14 @@
-// Search DB Model
+import { time } from "console";
+import mongoose, { Schema, Document } from "mongoose";
+
+interface ISearchLog extends Document {
+    query: string;
+    timestamp: Date;
+}
+
+const SearchLogSchema: Schema = new Schema({
+    query: { type: String, required: true },
+    timestamp: { type: Date, default: Date.now }
+});
+
+export default mongoose.model<ISearchLog>('SearchLog', SearchLogSchema);
